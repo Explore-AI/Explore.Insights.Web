36040ddb81701501b73fcfdb2edc954a5e80e760
Brijesh <brijeshjp2006@gmail.com>
2021-05-07 11:59:08 +0530

    Html code in Beta folder

--- beta/js/custom.js
+++ beta/js/custom.js
@@ -1,27 +1,86 @@
-jQuery(document).ready(function($){
-	    $(".right-menu > ul > li.menu-item-has-children > a").after('<span class="submenu-close"></span>');
-	    $('span.submenu-close').click(function() {
-	    $(this).next().slideToggle();
-		});
-		
-		$('.mobile-menu-icon').click(function() {
-			$("body").toggleClass("menu-show");
-			$(this).toggleClass("menu-icon-active");
-		});
-		
-		$('.analytics-officers-select > ul > li > a').click(function() {
-			$(this).toggleClass("select-item-show");
-			$(this).next().slideToggle()
-		});
-		
-		$('.analytics-officers-items li a').click(function() {
-			var get_value = $(this).html();
-			$(".analytics-officers-select > ul > li > a").html(get_value);
-			$(".analytics-officers-select > ul > li > a").toggleClass("select-item-show");
-			$("ul.analytics-officers-items").slideToggle()
-		});
-		
-		
+//menu-slide"
+$(".mobile-menu").on("click", function(){
+	$(".navbar").addClass('sidenav');
+	$(".main-header").addClass('mobile-nav');
+});
+$(".closebtn").on("click", function(){
+	$(".main-header").removeClass('mobile-nav');
+	$(".navbar").removeClass('sidenav');
+});
+$(".nav-drop span").on("click", function(){
+	$(".mobile-nav .nav-drop .right-submenu").slideToggle();
+});
+$(".pricing-nav-item li a").on("click", function(){
+	$(".pricing-nav-item li").removeClass('active');
+	$(this).parent().addClass('active');
+});
+$(".yearly-plan a").on("click", function(){
+	$(".pricing-plan-content").removeClass('active');
+	$(".yearly-pricing").addClass('active');
+});
+$(".monthly-plan a").on("click", function(){
+	$(".pricing-plan-content").removeClass('active');
+	$(".monthly-pricing").addClass('active');
+});
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+  }
+
+//scroll-header
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 1){  
+		$('.main-header').addClass("sticky");
+		$(this).slideDown(200);
+		}
+		else{
+			$('.main-header').removeClass("sticky");
+			$(".main-header").css("top", "0");
+		}
+	});
+	var $document = $(document),
+	$element = $('.main-header');
+
+	$document.scroll(function() {
+		if ($document.scrollTop() >= 180) {
+			$element.stop().css({
+				top: '0px'
+			});
+		} else {
+			$element.stop().css({
+				top: '-200px'
+			});
+		}
 	});
 
+//testimonials
+
+	$('.testimo-owl').owlCarousel({
+		loop:true,
+		margin: 0,
+		nav:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			768:{
+				items:2
+			}
+		}
+	});
+
+	$('.owl-logos').owlCarousel({
+		margin:10,
+		loop:true,
+		autoWidth:true,
+		dots: true,
+		items:1
+	})
 
